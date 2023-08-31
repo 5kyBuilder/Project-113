3fe2e032b858c57300f7e83408f37d05d44913bd
5kyBuilder <124388396+5kyBuilder@users.noreply.github.com>
2023-08-31 15:35:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,27 +10,14 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
-
-    tint_color = "";
 }
 
 function draw()
 {
     image(video, 0, 0, 500, 300);
-    tint(tint_color);
     rect(490, 0, 60, 300);
     rect(0, 0, 500, 10);
     rect(0, 0, 10, 300);
     rect(0, 290, 500, 10);
 
-}
-
-function apply()
-{
-    tint_color = document.getElementById("tint_color").value;
-}
-
-function take_snapshot()
-{
-    save("photo.png");
 }
